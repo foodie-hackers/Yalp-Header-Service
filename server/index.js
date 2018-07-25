4547dbf751570f838980a0f987015c684fbf12d4
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2018-07-25 12:31:16 -0700

    Connect server with static html page

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const db = require('../database-mysql/index.js');
+
 const app = express();
 const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
