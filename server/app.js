9ad5eddf59ba24890f980117f171c29c518484e1
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2018-07-27 17:43:21 -0700

    Implement express tests using jest
    Modify server/index.js by making a new file app.js and separating the app from the server. The reason for this is to prevent the error 'EADDRINUSE', which happens when a server is already running on the specified port number.
    
    Write two tests to test express.js. The first test is testing for the correct status code when the get request handler is invoked with a valid restaurant id. The second test is testing to see if the get request handler retrieves the correct information given the restaurant id.

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('../database-mysql/index.js');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/restaurants/:restaurantId', (req, res) => {
+  db.getRestaurantInfo(req.params.restaurantId, (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+module.exports = {
+  app,
+};
