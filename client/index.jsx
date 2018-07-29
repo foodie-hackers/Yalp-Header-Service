b74487b4629a11e3f28b151b026d41aa60de1cae
Tong Allan Zhao <allanzhao@tongs-air.attlocal.net>
2018-07-29 14:53:12 -0700

    Apply css for left side of header using styled components
    Modify classnames to styled components.
    
    Use font awesome to generate stars for average rating of restaurant.

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+import styled from 'styled-components';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar, faStarHalf, faCamera, faShareSquare, faBookmark } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faStar, faStarHalf, faCamera, faShareSquare, faBookmark);
 
 class App extends React.Component {
   constructor(props) {
@@ -29,27 +35,98 @@ class App extends React.Component {
   }
 
   render() {
+    const Div = styled.div`
+      background-color: white;
+      display: flex;
+      font-family: arial;
+    `;
+
+    const HeaderLeft = styled.div`
+      display: flex;
+      width: 50%;
+      padding-left: 20px;
+    `;
+
+    const HeaderRight = styled.div`
+      display: flex;
+      width: 50%;
+    `;
+
+    const RestaurantName = styled.div`
+      font-weight: bold;
+      font-size: 36px;
+      padding-top: 6px;
+      padding-bottom: 6px;
+      line-height: 1.16667em;
+      width: 100%;
+    `;
+
+    const ReviewInfo = styled.div`
+      display: flex;
+    `;
+
+    const AverageRating = styled.div`
+      float: left;
+      margin: -6px 6px 0 0;
+      padding-bottom: 6px;
+    `;
+
+    const rating = stars => ({
+      backgroundColor: stars > 3.5 ? 'red' : stars > 0 ? 'orange' : 'gray',
+      color: 'white',
+      margin: '2px',
+      padding: '4px',
+    });
+
+    const halfRating = stars => ({
+      background: stars > 3.5 ? 'linear-gradient(to right, red 50%, gray 50%)' : 'linear-gradient(to right, orange 50%, gray 50%)',
+      color: 'white',
+      margin: '2px',
+      padding: '4px',
+    });
+
+    const ReviewCount = styled.div`
+      display: flex;
+      padding: "4px";
+      color: #666;
+      font-weight: normal;
+    `;
+
+    const PriceRange = styled.div`
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      vertical-align: baseline;
+    `;
+
     return (
-      <div className="biz-page-header">
-        <div className="biz-page-header-left">
-          <div className="restaurant-name">
-            <h1>
-              {this.state.name}
-            </h1>
-          </div>
+
+      <Div className="biz-page-header">
+        <HeaderLeft>
           <div className="biz-main-info">
-            <div className="rating">
-              {this.state.averageRating}
-            </div>
-            <div className="reviews">
-              {this.state.reviewCount} reviews
-            </div>
-            <div className="priceRange">
+            <RestaurantName>
+              {this.state.name}
+            </RestaurantName>
+            <ReviewInfo>
+              <AverageRating>
+                {Array(5).fill().map((e, i) => (
+                  i + 0.5 === this.state.averageRating
+                    ? <FontAwesomeIcon icon="star" style={halfRating(this.state.averageRating)} />
+                    : <FontAwesomeIcon icon="star" style={i< this.state.averageRating ? rating(this.state.averageRating) : rating(0)} />
+                ))}
+              </AverageRating>
+              <ReviewCount>
+                {this.state.reviewCount} reviews
+              </ReviewCount>
+            </ReviewInfo>
+            <PriceRange>
               {this.state.priceRange === 1 ? '$' : this.state.priceRange === 2 ? '$$' : this.state.priceRange === 3 ? '$$$' : '$$$$'}
-            </div>
+            </PriceRange>
           </div>
-        </div>
-        <div className="biz-page-header-right">
+        </HeaderLeft>
+        <HeaderRight>
           <div className="write-review-button">
             <button>Write a Review</button>
           </div>
@@ -62,8 +139,8 @@ class App extends React.Component {
           <span className="save-button">
             <button>Save</button>
           </span>
-        </div>
-      </div>
+        </HeaderRight>
+      </Div>
     );
   }
 }
